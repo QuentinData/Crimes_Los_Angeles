1a2b56fe8ffddb11b2ac356674b9001eaa156a46
Quentin Goumeziane <123408068+QuentinData@users.noreply.github.com>
2024-01-04 19:55:38 +0100

    MAJ PROJET
    MAJ PROJET

--- app.R
+++ app.R
@@ -2,8 +2,230 @@ source(file = "global.r")
 source(file = "packages.r")
 
 
-ui <- fluidPage()
+# Interface utilisateur
 
-server <- function(input, output) {}
 
+ui <- fluidPage(
+  titlePanel("Les crimes par armes à feu dans Los Angeles en 2022"),
+  
+  tags$head(
+    tags$style(HTML("
+    body {
+      background-color: #f4f4f4;
+    }
+    .navbar {
+      background-color: #333;
+    }
+    .navbar-brand {
+      color: #fff !important;
+    }
+    .custom-main-panel {
+      background-color: #ffffff; /* Blanc */
+    }
+  "))
+  ),
+  
+  #Sidebar
+  
+  
+  sidebarLayout(
+    sidebarPanel(
+      
+      # Ajout d'un titre
+      
+      h2("Séléction des filtres", icon = icon("filter")),
+      
+      # Ajout des filtres sur différents critères
+      
+      selectInput("weaponFilter", "Filtrer par arme :", choices = unique(data$Arme), multiple = TRUE),
+      selectInput("areaFilter", "Filtrer par zone :", choices = unique(data$Lieu), multiple = TRUE),
+      selectInput("seasonFilter", "Filtrer par saison :", choices = unique(data$Saison), multiple = TRUE),
+      selectInput("momentFilter", "Filtrer par moment :", choices = unique(data$Moment), multiple = TRUE),
+      selectInput("statusFilter", "Filtrer par statut :", choices = unique(data$Etat), multiple = TRUE),
+      h5("Attention, veuillez faire au moins une séléction dans chaque filtre pour mettre à jours les visuels")
+      
+    ),
+    
+    #Mainpanel
+    
+    mainPanel(
+      class = "custom-main-panel",
+      tabsetPanel(
+        
+        #Onglet 1
+        
+        tabPanel("Données Initiales", icon = icon("table"),
+                 h2("Table de données initiale"),
+                 tableOutput("initialDataTable")
+        ),
+        
+        #Onglet 2
+        
+        tabPanel("Résumé", icon = icon("table"),
+                 h2("Résumé des affaires"),
+                 tableOutput("summaryTable"),
+                 
+                 h3("Répartition par zone"),
+                 tableOutput("areaTable"),
+                 
+                 h3("Répartition par arme"),
+                 tableOutput("weaponTable"),
+                 
+                 h3("Répartition par saison"),
+                 tableOutput("seasonTable"),
+                 
+                 h3("Répartition par moment"),
+                 tableOutput("momentTable")
+        ),
+        
+        #Onglet 3
+        
+        tabPanel("Repartition des états", icon = icon("chart-simple"),
+                 h2("Statistiques"),
+                 plotOutput("percentageUnresolvedCases")
+        ),
+        
+        #Onglet 4
+        
+        tabPanel("Distribution des âges", icon = icon("chart-column"),
+                 h2("Répartition des sexes des victimes par zone"),
+                 plotOutput("sexDistributionPlot")
+        ),
+        
+        #Onglet 5
+        
+        tabPanel("Carte Interactive", icon = icon("map"),
+                 h2("Carte interactive des crimes"),
+                 leafletOutput("densityMap")
+        )
+      )
+    )
+  )
+)
+
+
+
+# Serveur
+
+
+server <- function(input, output, session) {
+  
+  # Tableau de données initiales
+  output$initialDataTable <- renderTable({
+    data
+  })
+  
+  # Tableau résumé
+  output$summaryTable <- renderTable({
+    total_affaires <- nrow(filteredData())
+    summary_data <- data.frame(
+      Nombre = total_affaires
+    )
+    summary_data
+  })
+  
+  # Tableau de répartition par zone
+  output$areaTable <- renderTable({
+    total_affaires <- nrow(filteredData())
+    area_data <- filteredData() %>%
+      group_by(Lieu) %>%
+      summarise(Nombre = n()) %>%
+      arrange(desc(Nombre)) %>%
+      mutate(Pourcentage = (Nombre / total_affaires) * 100)
+    area_data
+  })
+  
+  # Tableau de répartition par arme
+  output$weaponTable <- renderTable({
+    total_affaires <- nrow(filteredData())
+    weapon_data <- filteredData() %>%
+      group_by(Arme) %>%
+      summarise(Nombre = n()) %>%
+      arrange(desc(Nombre)) %>%
+      mutate(Pourcentage = (Nombre / total_affaires) * 100)
+    weapon_data
+  })
+  
+  # Tableau de répartition par saison
+  output$seasonTable <- renderTable({
+    total_affaires <- nrow(filteredData())
+    season_data <- filteredData() %>%
+      group_by(Saison) %>%
+      summarise(Nombre = n()) %>%
+      arrange(desc(Nombre)) %>%
+      mutate(Pourcentage = (Nombre / total_affaires) * 100)
+    season_data
+  })
+  
+  # Tableau de répartition par moment
+  output$momentTable <- renderTable({
+    total_affaires <- nrow(filteredData())
+    moment_data <- filteredData() %>%
+      group_by(Moment) %>%
+      summarise(Nombre = n()) %>%
+      arrange(desc(Nombre)) %>%
+      mutate(Pourcentage = (Nombre / total_affaires) * 100)
+    moment_data
+  })
+  
+  filteredData <- reactive({
+    # Filtrage des données en fonction des sélections
+    selectedWeapon <- input$weaponFilter
+    selectedArea <- input$areaFilter
+    selectedSeason <- input$seasonFilter
+    selectedMoment <- input$momentFilter
+    selectedStatus <- input$statusFilter
+    
+    data %>%
+      filter(Arme %in% selectedWeapon,
+             Lieu %in% selectedArea,
+             Saison %in% selectedSeason,
+             Moment %in% selectedMoment,
+             Etat %in% selectedStatus)
+  })
+  
+  # Graphique à barres pour la répartition par état
+  output$percentageUnresolvedCases <- renderPlot({
+    ggplot(filteredData(), aes(x = Etat, fill = Etat)) +
+      geom_bar() +
+      labs(title = "Répartition des valeurs de Status.Desc",
+           x = "Statut",
+           y = "Nombre de cas") +
+      theme_minimal()
+  })
+  
+  # Histogramme de répartition des âges des victimes
+  output$sexDistributionPlot <- renderPlot({
+    ggplot(filteredData(), aes(x = Age)) +
+      geom_histogram(binwidth = 5, fill = "skyblue", color = "black", alpha = 0.7) +
+      labs(title = "Répartition des âges des victimes",
+           x = "Âge",
+           y = "Nombre de cas") +
+      theme_minimal()
+  })
+  
+  # Carte interactive
+  output$densityMap <- renderLeaflet({
+    heatmap_data <- filteredData() %>%
+      group_by(LAT, LON) %>%
+      summarize(count = n(), .groups = 'drop')
+    
+
+    if (length(heatmap_data$count) > 0) {
+      # Ajustement du facteur d'échelle
+      scale_factor <- max(heatmap_data$count) / 10
+    } else {
+      scale_factor <- 1
+    }
+    
+    # Carte avec des cercles proportionnels à la densité et une couleur de remplissage
+    leaflet() %>%
+      setView(lng = mean(filteredData()$LON), lat = mean(filteredData()$LAT), zoom = 10) %>%
+      addTiles() %>%
+      addCircles(lng = ~LON, lat = ~LAT, radius = ~count * scale_factor, fillOpacity = 1, color = "red", fill = TRUE, data = heatmap_data)
+  })
+}
+
+
+# Lancement de l'application Shiny
 shinyApp(ui = ui, server = server)
--- global.R
+++ global.R
@@ -1,1 +1,69 @@
+#importation des données
 data <- read.csv("DATA/Crime_Data_from_2020_to_Present.csv", header = TRUE, sep = ",")
+
+#filtre des colonnes
+data <- data[c("DATE.OCC", "TIME.OCC", "AREA.NAME", "Vict.Sex", "Weapon.Desc", "LAT", "LON", "Status.Desc", "Vict.Age")]
+
+#traitement de la colonne de date
+
+#Extraction de la date
+data$DATE.OCC <-  substr(data$DATE.OCC, 1, 10)
+
+#Extraction de l'année
+data$Annee <- substr(data$DATE.OCC, 7, 10)
+
+#Extraction du mois
+data$Mois <- substr(data$DATE.OCC, 1, 2)
+data$Mois <- as.numeric(data$Mois)
+
+#Création de la colonne Saison
+data$Saison <- cut(data$Mois, breaks = c(0, 3, 6, 9, 12), labels = c("Hiver", "Printemps", "Été", "Automne"))
+
+#Création de la colonne Moment
+data$Moment <- cut(data$TIME.OCC, breaks=c(0, 600, 1200, 1800, 2400), labels=c('nuit', 'matin', 'après-midi', 'soirée'))
+
+#Traitement de la colonnes des armes
+data$Weapon.Desc <- ifelse(data$Weapon.Desc %in% c("ASSAULT WEAPON/UZI/AK47/ETC", 
+                                                   "HECKLER & KOCH 93 SEMIAUTOMATIC ASSAULT RIFLE",
+                                                   "M-14 SEMIAUTOMATIC ASSAULT RIFLE",
+                                                   "MAC-11 SEMIAUTOMATIC ASSAULT WEAPON",
+                                                   "SEMI-AUTOMATIC RIFLE",
+                                                   "AUTOMATIC WEAPON/SUB-MACHINE GUN",
+                                                   "M1-1 SEMIAUTOMATIC ASSAULT RIFLE",
+                                                   "HECKLER & KOCH 91 SEMIAUTOMATIC ASSAULT RIFLE",
+                                                   "MAC-10 SEMIAUTOMATIC ASSAULT WEAPON",
+                                                   "SEMI-AUTOMATIC PISTOL",
+                                                   "UZI SEMIAUTOMATIC ASSAULT RIFLE"), "Armes automatiques", data$Weapon.Desc)
+
+data$Weapon.Desc <- ifelse(data$Weapon.Desc %in% c("AIR PISTOL/REVOLVER/RIFLE/BB GUN",
+                                                   "REVOLVER",
+                                                   "HAND GUN"), "Armes de poing", data$Weapon.Desc)
+
+data$Weapon.Desc <- ifelse(data$Weapon.Desc %in% c("UNKNOWN FIREARM",
+                                                   "SAWED OFF RIFLE/SHOTGUN",
+                                                   "ANTIQUE FIREARM",
+                                                   "SHOTGUN",
+                                                   "STUN GUN",
+                                                   "RELIC FIREARM"), "Autres armes à feu", data$Weapon.Desc)
+
+#Filtres des données
+data <- subset(data, Annee == "2022" & 
+                   (Vict.Sex == "F" | Vict.Sex == "M") & 
+                   (Weapon.Desc == "Autres armes à feu" | Weapon.Desc == "Armes de poing" | Weapon.Desc == "Armes automatiques") &
+                   (Vict.Age > 0))
+
+#Modification des noms de colonnes
+colnames(data)[colnames(data) == "Weapon.Desc"] <- "Arme"
+colnames(data)[colnames(data) == "AREA.NAME"] <- "Lieu"
+colnames(data)[colnames(data) == "TIME.OCC"] <- "Heure"
+colnames(data)[colnames(data) == "DATE.OCC"] <- "Date"
+colnames(data)[colnames(data) == "Vict.Sex"] <- "Sexe"
+colnames(data)[colnames(data) == "Vict.Age"] <- "Age"
+colnames(data)[colnames(data) == "Status.Desc"] <- "Etat"
+
+#Modification des valeurs de la colonne Etat
+data$Etat[data$Etat == "Invest Cont"] <- "En cours"
+data$Etat[data$Etat == "Adult Arrest"] <- "Adulte arrêté"
+data$Etat[data$Etat == "Adult Other"] <- "Adulte autres"
+data$Etat[data$Etat == "Juv Arrest"] <- "Mineur arrêté"
+data$Etat[data$Etat == "Juv Other"] <- "Mineur autres"
